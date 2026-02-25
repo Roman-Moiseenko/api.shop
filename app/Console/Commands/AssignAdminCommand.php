674b750babde2ee4394bb352a50739a3d6857895
DESKTOP-OSQO9VT\Роман <r.a.moiseenko@gmail.com>
2026-02-25 10:08:17 +0200

    1++
    Signed-off-by: DESKTOP-OSQO9VT\Роман <r.a.moiseenko@gmail.com>

---
+++ app/Console/Commands/AssignAdminCommand.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Console\Commands;
+
+
+use App\Modules\Auth\Entity\User;
+use App\Modules\Staff\Entity\Staff;
+use Illuminate\Console\Command;
+use Illuminate\Support\Str;
+
+class AssignAdminCommand extends Command
+{
+    protected $signature = 'admin:assign {name}';
+    protected $description = 'Command description';
+
+
+    public function handle(): bool
+    {
+        $name = $this->argument('name');
+        $email = $name . '@shop.api';
+
+        if (is_null($user = User::where('email', $name)->first())) {
+            $this->error('Пользователь не найден');
+            return false;
+        }
+        $user->assignRole('admin');
+
+        $this->info('Пользователю ' . $name . ' назначена роль admin');
+
+        return true;
+    }
+}
